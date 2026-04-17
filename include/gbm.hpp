5c1f0e9255641957c6f5f70d7ecc82b071c40649
moradiya84 <ommoradiya6p556@gmail.com>
2026-04-17 13:26:04 +0530

    improved personalized comments

--- include/gbm.hpp
+++ include/gbm.hpp
@@ -3,11 +3,16 @@
 #include <cmath>
 #include <vector>
 
-enum class Scheme { Euler, Milstein };
+enum class Scheme
+{
+  Euler,
+  Milstein
+};
 
-struct GBMConfig {
+struct GBMConfig
+{
   size_t num_assets;
-  size_t num_paths;
+  size_t num_paths; // Number of monte ccarlo simulation
   size_t num_steps;
   double T;  // Maturity time
   double dt; // Time step size = T / num_steps
@@ -25,7 +30,8 @@ struct GBMConfig {
 // GBM Class:
 // Stores parameters, provides drift/diffusion functions, and the exact
 // solution.
-class GBM {
+class GBM
+{
 private:
   GBMConfig config_;
 
@@ -36,20 +42,23 @@ public:
 
   // Drift term for asset i given current price S
   // a(t, S) = mu_i * S
-  double drift(size_t asset_idx, double S) const {
+  double drift(size_t asset_idx, double S) const
+  {
     return config_.mu[asset_idx] * S;
   }
 
   // Diffusion term for asset i given current price S
   // b(t, S) = sigma_i * S
-  double diffusion(size_t asset_idx, double S) const {
+  double diffusion(size_t asset_idx, double S) const
+  {
     return config_.sigma[asset_idx] * S;
   }
 
   // Derivative of the diffusion term with respect to S
   // b'(t, S) = sigma_i
   // (Used in the Milstein scheme)
-  double diffusion_derivative(size_t asset_idx, double /*S*/) const {
+  double diffusion_derivative(size_t asset_idx, double /*S*/) const
+  {
     return config_.sigma[asset_idx];
   }
 
@@ -59,7 +68,7 @@ public:
        now let's assume some function f(s) = ln(s)
        then df = (1/s)ds - (1/2)*(1/s^2)ds^2
        substitute ds = mu*s*dt + sigma*s*dW_t
-       now in ds^2 dw^2 term will become dt because of quadratic variation of
+       now in ds^2 , dw^2 term will become dt because of quadratic variation of
      Brownian motion
 
        df = mu*dt + sigma*dW_t - (1/2)*sigma^2*dt
@@ -71,7 +80,8 @@ public:
        s_t = s_0*exp((mu - 0.5*sigma^2)*t + sigma*W_t)
   */
   // S_t^(i) = S_0^(i) * exp((mu_i - 0.5 * sigma_i^2) * t + sigma_i * W_t)
-  double exact_solution(size_t asset_idx, double t, double W_t) const {
+  double exact_solution(size_t asset_idx, double t, double W_t) const
+  {
     double mu = config_.mu[asset_idx];
     double sigma = config_.sigma[asset_idx];
     double S0 = config_.S0[asset_idx];
@@ -80,7 +90,8 @@ public:
   }
 
   // Exact theoretical solution for asset i at maturity T
-  double exact_solution_T(size_t asset_idx, double W_T) const {
+  double exact_solution_T(size_t asset_idx, double W_T) const
+  {
     return exact_solution(asset_idx, config_.T, W_T);
   }
 };
